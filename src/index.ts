16585258f441a891f567978669565992c27156ff
Oleg Valter <o.a.philippov@gmail.com>
2021-05-21 08:53:54 +0300

    chores: linted and fixed npm script

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,38 @@ interface MouseEvent {
 }
 
 ((_w, d) => {
-  const makeReplacement = () => {
-    const replacer = d.createElement("i");
-    replacer.textContent = "removed";
-    return replacer;
-  };
-
-  const initRemoving = () => {
-    d.addEventListener(
-      "click",
-      ({ target }) => {
-        if (!target?.classList.contains("content")) return;
-        while (target.firstElementChild) target.firstElementChild.remove();
-        target.append(makeReplacement());
-      },
-      { once: true }
-    );
-  };
-
-  d.addEventListener("keyup", ({ ctrlKey, metaKey, key }) => {
-    if (!ctrlKey && !metaKey) return;
-
-    const shortcutMap: { [x: string]: () => void } = {
-      R: initRemoving,
+    const makeReplacement = () => {
+        const replacer = d.createElement('i');
+        replacer.textContent = 'removed';
+        return replacer;
     };
 
-    return shortcutMap[key.toUpperCase()]();
-  });
+    const initRemoving = () => {
+        d.addEventListener(
+            'click',
+            ({ target }) => {
+                if (!target?.classList.contains('content')) return;
+                while (target.firstElementChild) target.firstElementChild.remove();
+                target.append(makeReplacement());
+            },
+            { once: true }
+        );
+    };
 
-  d.addEventListener("click", (event) => {
-    const { target } = event;
-    if (!target || target.id !== "footer-logo") return;
-    event.preventDefault();
-    return initRemoving();
-  });
+    d.addEventListener('keyup', ({ ctrlKey, metaKey, key }) => {
+        if (!ctrlKey && !metaKey) return;
+
+        const shortcutMap: { [x: string]: () => void } = {
+            R: initRemoving,
+        };
+
+        return shortcutMap[key.toUpperCase()]();
+    });
+
+    d.addEventListener('click', (event) => {
+        const { target } = event;
+        if (!target || target.id !== 'footer-logo') return;
+        event.preventDefault();
+        return initRemoving();
+    });
 })(window, document);
